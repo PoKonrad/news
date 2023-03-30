1e35c2abe8756a253393376939428ba011e52d32
Konrad Porębski <110181064+PoKonrad@users.noreply.github.com>
2023-03-30 13:18:03 +0200

    refactor: removed unused imports

--- gnNews/src/components/Header/Header.tsx
+++ gnNews/src/components/Header/Header.tsx
@@ -1,38 +1,11 @@
-import {
-  AppBar,
-  Button,
-  IconButton,
-  ToggleButton,
-  ToggleButtonGroup,
-  Toolbar,
-  Typography
-} from '@mui/material';
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import { Box } from '@mui/system';
-import { blue } from '@mui/material/colors';
-import InfoIcon from '@mui/icons-material/Info';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectViewMode, setViewMode } from '../../features/viewModeSlice';
+import { useDispatch } from 'react-redux';
 import { toggleSidePanel } from '../../features/sidePanelSlice';
-import LanguageSelection from './LanguageSelection';
-import DisplayButton from './DisplayToggleButtons';
-import DisplayToggleButtons from './DisplayToggleButtons';
 import MobileMenu from './MobileMenu';
 
 const Header = () => {
-  const mode = useSelector(selectViewMode);
   const dispatch = useDispatch();
-
-  const handleModeChange = (event: React.MouseEvent<HTMLElement>, newMode: string) => {
-    if (newMode === null) {
-      return;
-    }
-    dispatch(
-      setViewMode({
-        newMode: newMode
-      })
-    );
-  };
 
   const handleMenuClick = () => {
     dispatch(toggleSidePanel());
